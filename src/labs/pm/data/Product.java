38df843f32934c1abfcd80919960a81359e4e726
mohammedsabouni <mohammed.sabouni.2002@gmail.com>
2025-02-23 22:28:44 +0000

    class and object practices

---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2025.
+ */
+
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * {@code Product} class represents properties and behaviors of product objects in the Product Management System.
+ * <br>
+ * Each product has an id, name, and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ * @version 4.0
+ * @author Sabouni Mohammed Amine
+ **/
+public class Product {
+    private int id;
+    private String name;
+    private BigDecimal price;
+    /**
+     * A constant that defines a
+     * {@link java.math.BigDecimal BigDecimal} value of the discount rate
+     * <br>
+     * Discount rate is 10%
+     */
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(final BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    /**
+     * Calculates discount based on a product price and
+     * {@link DISCOUNT_RATE discount rate}
+     * @return a {@link java.math.BigDecimal BigDecimal}
+     * value of the discount
+     */
+    public BigDecimal getDiscount(){
+        return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+}
